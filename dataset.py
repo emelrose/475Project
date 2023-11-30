888093e030da3df6152ae2d21de0c9d183087921
paulina-01 <paufk2001@gmail.com>
2023-11-29 22:11:07 -0500

    wednesday night final output

--- dataset.py
+++ dataset.py
@@ -7,52 +7,76 @@ from PIL import Image
 import cv2
 
 
-def resize_image(image, scale_percent):
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-
-    resized_image = cv2.resize(image, (width, height))
-
+def resize_image(image, target_size):
+    resized_image = cv2.resize(image, target_size)
     return resized_image
 
 
-def resize_point(original_point, scale_percent):
+def resize_point(original_point, image, target_size):
+    # Get the original image size
+    original_image_size = (image.shape[1], image.shape[0])
+
+    # Calculate scale factors for both dimensions
+    scale_factor_x = target_size[1] / original_image_size[1]
+    scale_factor_y = target_size[0] / original_image_size[0]
+
     # Scale the coordinates of the point
     x, y = original_point
-    scaled_x = int(x * scale_percent / 100)
-    scaled_y = int(y * scale_percent / 100)
+    scaled_x = int(x * scale_factor_x)
+    scaled_y = int(y * scale_factor_y)
 
     return scaled_x, scaled_y
 
 
+def draw_keypoints(image, nose):
+    for point in nose:
+        x, y = point
+        cv2.circle(image, (x, y), 2, (0, 0, 255), 2)  # Draw a red circle around the keypoints
+        # cv2.circle(image, center_coordinates, radius, color, thickness)
+    return image
+
+
 class PetDataset(Dataset):
-    def __init__(self, txt, root_dir, transform=None, scale_percent=100):
-        self.labels = pd.read_csv(txt)
+    def __init__(self, txt, root_dir, transform=None, target_size=(640, 640)):
+        self.labels = pd.read_csv(txt)  # makes sense
         self.root_dir = root_dir
         self.transform = transform
-        self.scale_percent = scale_percent
+        self.target_size = target_size
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.labels)  # makes sense
 
     def __getitem__(self, idx):
-        #img_name = os.path.join(self.root_dir, f'image_{idx}.jpg')
+        # check for indexing operations (can delete if causing issues)
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        # Get item from folder
         image_name_parts = self.labels.iloc[idx, 0].split('_')
         breed_name = "_".join(image_name_parts[:-1])  # Join all parts except the last one
         img_name = os.path.join(self.root_dir, f'{breed_name}_{image_name_parts[-1]}')
-        image = cv2.imread(img_name) #this works
+
+        # Read image
+        image = cv2.imread(img_name)  # this works
 
         # Resize the image
-        image = resize_image(image, self.scale_percent)
+        image = resize_image(image, self.target_size)
 
         # Convert nose coordinates to a tensor
         nose_str = self.labels.iloc[idx, 1][1:-1]
         original_nose = list(map(int, nose_str.split(',')))
-        nose = resize_point(original_nose, self.scale_percent)
+        nose = resize_point(original_nose, image, self.target_size)
         nose = torch.tensor(nose, dtype=torch.float32)
+        sample = {'image': image, 'nose': nose}
 
         if self.transform:
             image = Image.fromarray(image)
             image = self.transform(image)
 
-        return image, nose
+        return sample
+
+
+    def visualize_keypoints(self, image, nose):
+        # Draw keypoints on the image
+        img_with_keypoints = draw_keypoints(self, nose)
+        return img_with_keypoints
--- test_dataset.py
+++ test_dataset.py
@@ -3,24 +3,18 @@ import torch
 from torchvision import transforms
 from dataset import PetDataset
 import torch.utils.data
+import os
+import matplotlib.pyplot as plt
 
+def draw_keypoints(image, noses):
+    # Convert the tensor to a numpy array and extract coordinates
+    x = int(noses[0].item())
+    y = int(noses[1].item())
 
-def resize_image(image, scale_percent):
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
+    # Draw a red circle around the keypoints
+    image = cv2.circle(image, (x, y), 2, (0, 0, 255), 2)
 
-    resized_image = cv2.resize(image, (width, height))
-
-    return resized_image
-
-
-def resize_point(original_point, scale_percent):
-    # Scale the coordinates of the point
-    x, y = original_point
-    scaled_x = int(x * scale_percent / 100)
-    scaled_y = int(y * scale_percent / 100)
-
-    return scaled_x, scaled_y
+    return image
 
 
 if __name__ == "__main__":
@@ -30,27 +24,51 @@ if __name__ == "__main__":
 
     # Set scale_percent to the desired value for resizing
     train_dataset = PetDataset(txt='./data/train_noses.3.txt', root_dir='./data/images/', transform=transform,
-                               scale_percent=50)
-    test_dataset = PetDataset(txt='./data/test_noses.txt', root_dir='./data/images/', transform=transform, scale_percent=50)
-    # Create DataLoader for training
-    train_batch_size = 32
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
-
+                               target_size=(640, 640))
+    test_dataset = PetDataset(txt='./data/test_noses.txt', root_dir='./data/images/', transform=transform,
+                              target_size=(640, 640))
     # Create DataLoader for testing
     test_batch_size = 32
     test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=test_batch_size, shuffle=False)
 
-    # Test DataLoader by printing the first batch
-    for batch in train_dataloader:
-        images, noses = batch
-        print("Train Batch:")
-        print("Images Shape:", images.shape)
-        print("Noses Shape:", noses.shape)
-        break  # Only print the first batch for brevity
+    # Save the first few images to the current working directory
+    output_dir = './output_images'
+    os.makedirs(output_dir, exist_ok=True)
 
+
+        # Test DataLoader by printing the first batch
     for batch in test_dataloader:
         images, noses = batch
         print("Test Batch:")
         print("Images Shape:", images.shape)
         print("Noses Shape:", noses.shape)
         break  # Only print the first batch for brevity
+
+    for images, noses in test_dataloader:
+        #images, noses = batch
+        i = 0
+
+        for image, nose in zip(images, noses):
+          i += 1
+          image_i = image.numpy()
+          image_i = cv2.imread(img_name)
+          print(image_i)
+          img_with_keypoints = draw_keypoints(image_i, nose)
+
+
+        # for i in range(images.size(0)): #so per batch
+        #   # Access the i-th image and nose from the batch
+        #   image_i = images[i].permute(1, 2, 0).numpy()
+        #   image_i = transforms.ToPILImage()(images[i])
+        #   nose_i = noses[i]
+
+        #   # Draw keypoints on the image
+        #   img_with_keypoints = draw_keypoints(image_i, nose_i)
+          # Save the image with keypoints
+          img_name = f"output_image_{i}_with_keypoints.png"
+          img_path = os.path.join(output_dir, img_name)
+          cv2.imwrite(img_path, cv2.cvtColor(img_with_keypoints, cv2.COLOR_RGB2BGR))
+          print(f"Saved {img_path}")
+
+        if i == 32:  # Break after processing the first batch for brevity
+            break
