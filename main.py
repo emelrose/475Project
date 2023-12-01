373121bd7fc7eec62ad23e199326d1243034241a
evanmelrose <evan.melrose4@gmail.com>
2023-12-01 18:01:22 -0500

    added train_main

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-
+import model as net
+from torchsummary import summary
+from train import train
 if __name__ == '__main__':
-    print("Hello")
+
+    num_epochs = 1
+    batch_size = 32
+    model = net.VetNet()
+    device = 'cpu'
+    learning_rate = 0.001
+    scaling_factor = 0.1
+    patience = 3
+    train(num_epochs, batch_size, model, device, learning_rate, scaling_factor, patience)
+
