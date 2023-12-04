3c79d6cfdd7c573f4617a9b8ac2503de0c566f27
paulina-01 <paufk2001@gmail.com>
2023-12-04 18:55:05 -0500

    added other corrupt files to remove

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from torchsummary import summary
 from train import train
 import os
 if __name__ == '__main__':
-    #os.remove("./data/images/Egyptian_Mau_167.jpg")
-    #os.remove("./data/images/Egyptian_Mau_14.jpg")
-    #os.remove("./data/images/Egyptian_Mau_139.jpg")
-    #os.remove("./data/images/Egyptian_Mau_145.jpg")
-    #os.remove("./data/images/Egyptian_Mau_156.jpg")
-    #os.remove("./data/images/Egyptian_Mau_177.jpg")
-    #os.remove("./data/images/Egyptian_Mau_186.jpg")
-    #os.remove("./data/images/Egyptian_Mau_191.jpg")
-    #os.remove("./data/images/Abyssinian_5.jpg")
-    #os.remove("./data/images/Abyssinian_34.jpg")
-    num_epochs = 20
-    batch_size = 20
+    #os.remove("./data/images/Egyptian_Mau_167.jpg") #DONE
+    #os.remove("./data/images/Egyptian_Mau_14.jpg") #DONE
+    #os.remove("./data/images/Egyptian_Mau_139.jpg") #DONE (not in train/test)
+    #os.remove("./data/images/Egyptian_Mau_145.jpg") #DONE (not in train/test)
+    #os.remove("./data/images/Egyptian_Mau_156.jpg") #DONE
+    #os.remove("./data/images/Egyptian_Mau_177.jpg") #DONE (not in train/test)
+    #os.remove("./data/images/Egyptian_Mau_186.jpg") #DONE
+    #os.remove("./data/images/Egyptian_Mau_191.jpg") #DONE (not in train/test)
+    #os.remove("./data/images/Abyssinian_5.jpg") #DONE
+    #os.remove("./data/images/Abyssinian_34.jpg") #DONE - preprocessing complete
+    #os.remove("./data/images/chihuahua_121.jpg") #alaso corrupted image
+    #os.remove("./data/image/beagle_116.jpg") #also corrupted image
+    num_epochs = 1
+    batch_size = 1
     model = net.VetNet()
     device = 'cuda'
-    learning_rate = 0.001
+    learning_rate = 0.005
     scaling_factor = 0.1
     patience = 3
     train(batch_size, num_epochs, model, device, learning_rate, scaling_factor, patience)
