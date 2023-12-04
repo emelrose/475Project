4d789f19f69b583bfdd7320d7bd371de374de316
evanmelrose <evan.melrose4@gmail.com>
2023-12-04 16:52:14 -0500

    added main.py to remove bad imgs

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import model as net
 from torchsummary import summary
 from train import train
+import os
 if __name__ == '__main__':
-
-    num_epochs = 1
-    batch_size = 32
+    #os.remove("./data/images/Egyptian_Mau_167.jpg")
+    #os.remove("./data/images/Egyptian_Mau_14.jpg")
+    #os.remove("./data/images/Egyptian_Mau_139.jpg")
+    #os.remove("./data/images/Egyptian_Mau_145.jpg")
+    #os.remove("./data/images/Egyptian_Mau_156.jpg")
+    #os.remove("./data/images/Egyptian_Mau_177.jpg")
+    #os.remove("./data/images/Egyptian_Mau_186.jpg")
+    #os.remove("./data/images/Egyptian_Mau_191.jpg")
+    #os.remove("./data/images/Abyssinian_5.jpg")
+    #os.remove("./data/images/Abyssinian_34.jpg")
+    num_epochs = 20
+    batch_size = 20
     model = net.VetNet()
-    device = 'cpu'
+    device = 'cuda'
     learning_rate = 0.001
     scaling_factor = 0.1
     patience = 3
-    train(num_epochs, batch_size, model, device, learning_rate, scaling_factor, patience)
+    train(batch_size, num_epochs, model, device, learning_rate, scaling_factor, patience)
+
 
