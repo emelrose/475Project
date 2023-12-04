e76d1ec4f2acaa15656aebadd6c0ae4251c8bc4f
paulina-01 <paufk2001@gmail.com>
2023-12-04 18:53:39 -0500

    Added plotting code and cleaned up (pls leave as is)

--- train.py
+++ train.py
@@ -1,13 +1,23 @@
 import statistics
 from pathlib import Path
+import cv2
 import dataset
 import os
 import argparse
+import time
 import torch
 import torch.nn as nn
 from model import VetNet
+from PIL import Image, ImageFile
+import matplotlib.pyplot as plt
+
+# Allow large images (640 x 640?)
+Image.MAX_IMAGE_PIXELS = None
+# Disable OSError: image file is truncated
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
+# determines the center point of the bounding box returned by yolo
 def get_output_center(x_min, y_min, x_max, y_max):
     # find the length and width of the returned bounding box
     x_length = x_max - x_min
@@ -20,21 +30,32 @@ def get_output_center(x_min, y_min, x_max, y_max):
     return x_center, y_center
 
 
-def distance_statistics(distances):
-    l_dist = []
-    for d in distances:
-        l_dist.append(d.item())
-        minimum_distance = min(l_dist)
-        maximum_distance = max(l_dist)
-        mean_distance = statistics.mean(l_dist)
-        standard_deviation = statistics.stdev(l_dist, mean_distance)
+# calculate euclidian distance of 2 points
+# def euclidian_distance(model_outputs, label_outputs):
+#     x1, x2 = model_outputs[0,0], label_outputs[0]
+#     y1, y2 = model_outputs[0,1], label_outputs[1]
+#     x_component = (x2-x1)**2
+#     y_component = (y2-y1)**2
+#     distance = math.sqrt(x_component + y_component)
+#     return distance
 
-        return minimum_distance, maximum_distance, mean_distance, standard_deviation
+def euclidian_distance(model_outputs, true_labels):  # Also calculates euclidean distance
+    # Assuming model_outputs and true labels are tensors of shape (batch_size, 2)
+    return torch.norm(model_outputs - true_labels, dim=1)
+
+
+def distance_statistics(distances):
+    minimum_distance = min(distances)
+    maximum_distance = max(distances)
+    mean_distance = statistics.mean(distances)
+    standard_deviation = statistics.stdev(distances, mean_distance)
+
+    return minimum_distance, maximum_distance, mean_distance, standard_deviation
 
 
 # function to build optimizer and lr_scheduler
 def build_train_elements(model, learning_rate, lr_scaling_factor, scheduler_patience):
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=0.005)  # apparently works better
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optimizer,
         factor=lr_scaling_factor,
@@ -47,46 +68,70 @@ def train(batch_size, num_epochs, model, device, learning_rate, lr_scaling_facto
     print("Training")
     data_loader = dataset.create_dataloader(batch_size, "Train")
     model = model.to(device=device)
+    criterion = nn.L1Loss()
     optimizer, scheduler = build_train_elements(
         model,
         learning_rate,
         lr_scaling_factor,
         scheduler_patience
     )
-    min, max, mean, std = 0, 0, 0, 0
-    t_distances = []
+
+    # define values
+    train_loss_values = []
+    val_loss_values = []
+
     for epoch_number in range(num_epochs):
+        start_time = time.time()
+        running_loss = 0
         epoch_distances = []
-        b_num = 0
-        for image_batched, label_batched in data_loader:
-            b_num += 1
-            d_length = len(data_loader)
-            batch_loss = []
-            if b_num % 10 == 0:
-                print(f"batcH: {b_num} of {d_length}")
-            if image_batched is None:
-                print(f"bad batche {b_num}")
-                continue
+        for batch in data_loader:
+            images, noses = batch
+            batch_images = images.to(device=device)
+            batch_labels = noses.to(device=device)
             optimizer.zero_grad()
-            batch_images = image_batched.to(device=device)
-            batch_labels = label_batched.to(device=device)
+
+            # Forward pass
             outputs = model(batch_images)
-            loss = torch.sqrt((outputs - batch_labels) ** 2).sum()
+
+            # Move the labels to the same device as the outputs
+            batch_labels = batch_labels.to(device=device)
+
+            # Calculate Loss
+            # loss = torch.sqrt((outputs - batch_labels) ** 2).sum()
+            loss = criterion(outputs, batch_labels)  # Replaced by L1Loss
             loss.backward()
             optimizer.step()
             # Calculate Euclidean_distance
-            # distance = euclidian_distance(outputs, batch_labels)
-            epoch_distances.append(loss.item())  # per batch, convert to list
-            torch.cuda.empty_cache()  # Clear GPU memory
-        # Convert list to tensor, per epoch
+            distance = euclidian_distance(outputs, batch_labels)
+            epoch_distances.extend(distance.tolist())  # per batch, convert to list
+            running_loss += loss.item()
 
-        for d in epoch_distances:
-            t_distances.append(d)
-        print(t_distances)
-        min, max, mean, std = distance_statistics(t_distances)
-        print(f"Total Stats: {min}, {max}, {mean}, {std}")
-        print(f"Epoch {epoch_number + 1}, Mean Distance: {epoch_distances.mean()}")
-        print("Epoch training Complete")
+        torch.cuda.empty_cache()  # Clear GPU memory
+
+        # Convert list to tensor, per epoch
+        epoch_distances = torch.tensor(epoch_distances, device=device)
+        avg_loss = running_loss / len(data_loader)
+        train_loss_values.append(avg_loss)
+        scheduler.step(avg_loss)
+
+        # results for every epoch
+        plt.figure(figsize=(10, 5))
+        plt.plot(train_loss_values, label='train')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.title('Training Losses')
+        plt.legend()
+        plt.savefig('./results_graph.png')
+        plt.close()
+
+        print(f"Epoch {epoch_number + 1}, Mean Distance: {epoch_distances.mean().item()}, Average Loss: {avg_loss} ")
+        save_dir = "Epoch_" + str(epoch_number) + "decoder.pth"
+        torch.save(model.state_dict(), save_dir)
+
+        end_time = time.time()
+        print(f"Epoch training Complete")
+        print(f"Time spent: {(end_time - start_time) / 60.00:.2f} minutes")
+        print(f"")
 
 
 def evaluate(batch_size, model, device):
@@ -104,9 +149,12 @@ def evaluate(batch_size, model, device):
             # Forward pass
             outputs = model(batch_images)
 
+            # Move the labels to the same device as the outputs
+            batch_labels = batch_labels.to(device=device)
+
             # Calculate Euclidean_distance
-            # distance = euclidian_distance(outputs, batch_labels)
-            # distances.extend(distance.tolist())  # per batch, convert to list
+            distance = euclidian_distance(outputs, batch_labels)
+            distances.extend(distance.tolist())  # per batch, convert to list
 
     # Convert list to tensor, per epoch
     distances_tensor = torch.tensor(distances, device=device)
@@ -132,7 +180,6 @@ def main(args):
         model.train()
         train(batch_size=args.b, num_epochs=args.e, model=model, device=device,
               learning_rate=args.gamma, lr_scaling_factor=0.1, scheduler_patience=4)
-        print("training complete!")
     elif args.mode == "test":
         evaluate(batch_size=args.b, model=model, device=device)
 
